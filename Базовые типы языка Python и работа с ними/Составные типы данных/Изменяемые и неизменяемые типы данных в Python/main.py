0b197109e7c0900def65b77e642fa0be8c53f81f
itkiber <itkiber@gmail.com>
2023-10-19 20:40:53 +0300

    task of changed type resolved

--- Базовые типы языка Python и работа с ними/Составные типы данных/Изменяемые и неизменяемые типы данных в Python/main.py
+++ Базовые типы языка Python и работа с ними/Составные типы данных/Изменяемые и неизменяемые типы данных в Python/main.py
@@ -1,1 +1,9 @@
-# TODO исправьте опечатку в слове
+fruits = ["яблоко", "банан", "опельсин", "виноград"]
+
+incorrect_word = fruits[2]
+
+correct_word = "а" + incorrect_word[1:]
+
+fruits[2] = correct_word
+
+print(fruits)
