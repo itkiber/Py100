c7fc88510a1f41d6b7428fd8060f8909b9529338
alemark <itkiber@gmail.com>
2023-10-19 19:09:34 +0300

    traffic task of basics types are resolved

--- Базовые типы языка Python и работа с ними/Лабораторные работы/Статистика посещений сайта/main.py
+++ Базовые типы языка Python и работа с ними/Лабораторные работы/Статистика посещений сайта/main.py
@@ -1,3 +1,3 @@
-users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
+users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2'] # Done
 
 # TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
